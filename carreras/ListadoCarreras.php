28bf731ae406a7e9704e6c90ab8008461c96cf5b
most025 <cristianmostazo2018@gamil.com>
2024-07-03 16:10:58 -0300

    actulizacion del listado

--- carreras/ListadoCarreras.php
+++ carreras/ListadoCarreras.php
@@ -20,6 +20,7 @@
     */
     ?>
     <main>
+        <!--Menu del modulo carrera-->
         <ul>
             <li><b><a href="../index.html">Inicio</a></b></li>
             <li><b><a href="../carreras/crearCarrera.html">Crear Carrera</a></b></li>
@@ -29,43 +30,41 @@
         </ul>
     </main>
     <br>
-    
+
     <div>
         <br>
-    <table border="1">
-        <thead id="color1">
-            <tr>
-                
-                <th>Nombre de la carrera</th>
-                <th>Abreviatura</th>
-                
-            </tr>
-        </thead>
+        <table border="1">
+            <thead id="color1">
+                <tr>
+                    <th>Nombre de la carrera</th>
+                    <th>Abreviatura</th>
+                </tr>
+            </thead>
 
-        <tbody>
-            <?php
-            while ($filas = mysqli_fetch_assoc($resultado)) {
-                /*
+            <tbody>
+                <?php
+                while ($filas = mysqli_fetch_assoc($resultado)) {
+                    /*
                 mysqli_fetch_assoc($coman): Esta función devuelve una fila de resultados como un array asociativo. 
                 $coman es el resultado de la consulta a la base de datos que se realizó anteriormente.
                 
                 */
 
-            ?>
-                <tr>
-                    
-                    <td><?php echo $filas['nombre'] ?></td>
-                    <td><?php echo $filas['descripcion']; ?></td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
+                ?>
+                    <tr>
+
+                        <td><?php echo $filas['nombre'] ?></td>
+                        <td><?php echo $filas['descripcion']; ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
 
 
 
-    </table>
-   </div>
+        </table>
+    </div>
     <?php
     //Esta función cierra una conexión previamente abierta a una base de datos.
     mysqli_close($conexion);
