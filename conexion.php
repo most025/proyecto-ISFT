4aef609929cd898148014027f8fa9b7bb77a674c
Cristian Mostazo <143824961+most025@users.noreply.github.com>
2025-10-10 15:35:09 -0300

    prueba

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@ $clave = "";
 $bd = "abm_escuela";
 
 $conexion = mysqli_connect($servidor, $usuario, $clave, $bd);
-
+// conexion a la base de datos
 if ($conexion->connect_error) {
 	die("Conexion fallida: " . $conexion->connect_error);
 } else {
